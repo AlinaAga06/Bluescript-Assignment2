47818c1fb549b58f966c7be55a2dae1a2e9ce8f9
AlinaAga06 <141228720+AlinaAga06@users.noreply.github.com>
2023-09-09 22:10:35 +0530

    Create Marvel.js

---
+++ Marvel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MarvelText from './MarvelText';
+
+function App() {
+  return (
+    <div className="App">
+      <MarvelText />
+      {/* Other content goes here */}
+    </div>
+  );
+}
+
+export default App;
