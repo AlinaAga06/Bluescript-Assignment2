a3b1142949cf534f02ee3fe50cfb13f7d6ec0dc0
AlinaAga06 <141228720+AlinaAga06@users.noreply.github.com>
2023-09-10 19:35:22 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
-import './MarvelText.css';
+import React, { useState } from "react";
+import "./MarvelText.css";
 
 function MarvelText() {
-  const [activeTab, setActiveTab] = useState('characters'); // Default active tab is "Characters"
+  const [activeTab, setActiveTab] = useState("characters"); // Default active tab is "Characters"
+  const [orderBy, setOrderBy] = useState("name"); // Default order is "Name"
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
+  };
+
+  const handleOrderByChange = (value) => {
+    setOrderBy(value);
   };
 
   return (
@@ -14,45 +19,74 @@ function MarvelText() {
       <div className="tabs-container">
         <div className="tab-row">
           <button
-            className={`tab-button ${activeTab === 'characters' ? 'active' : ''}`}
-            onClick={() => handleTabClick('characters')}
+            className={`tab-button ${
+              activeTab === "characters" ? "active" : ""
+            }`}
+            onClick={() => handleTabClick("characters")}
           >
             Characters
           </button>
           <div className="tab-spacer"></div>
           <button
-            className={`tab-button ${activeTab === 'comics' ? 'active' : ''}`}
-            onClick={() => handleTabClick('comics')}
+            className={`tab-button ${activeTab === "comics" ? "active" : ""}`}
+            onClick={() => handleTabClick("comics")}
           >
             Comics
           </button>
         </div>
         <div className="tab-row">
           <button
-            className={`tab-button ${activeTab === 'search' ? 'active' : ''}`}
-            onClick={() => handleTabClick('search')}
+            className={`tab-button ${activeTab === "search" ? "active" : ""}`}
+            onClick={() => handleTabClick("search")}
           >
             🔍Search
           </button>
         </div>
       </div>
+      <div className="sidebar">
+        <h2>Order By</h2>
+        <div className="order-radio">
+          <label>
+            <input
+              type="radio"
+              name="orderBy"
+              value="name"
+              checked={orderBy === "name"}
+              onChange={() => handleOrderByChange("name")}
+            />
+            Name
+          </label>
+        </div>
+        <div className="order-radio">
+          <label>
+            <input
+              type="radio"
+              name="orderBy"
+              value="modified"
+              checked={orderBy === "modified"}
+              onChange={() => handleOrderByChange("modified")}
+            />
+            Modified
+          </label>
+        </div>
+      </div>
       <div className="content">
-        {activeTab === 'characters' && (
+        {activeTab === "characters" && (
           <div>
             <h2>Characters</h2>
-            {/* Add your Characters content here */}
+            {/* Add  characters content  */}
           </div>
         )}
-        {activeTab === 'comics' && (
+        {activeTab === "comics" && (
           <div>
             <h2>Comics</h2>
-            {/* Add your Comics content here */}
+            {/* Add  comics content  */}
           </div>
         )}
-        {activeTab === 'search' && (
+        {activeTab === "search" && (
           <div>
             <h2>Search</h2>
-            {/* Add your Search content here */}
+            {/* Add  search content  */}
           </div>
         )}
       </div>
